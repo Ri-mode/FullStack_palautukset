a9092862b81477530671cfd4d73ba7d9e8fb64ea
Antti Kantola <antti.kantola@helsinki.fi>
2021-07-29 22:48:14 +0300

    osa1.8 cleaned!

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -33,15 +33,6 @@ const App = () => {
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
 
       <Statistics good={good} neutral={neutral} bad={bad} />
-
-      <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {good + neutral + bad}</p>
-      <p>Average {(good - bad) / (good + neutral + bad)}</p>
-      <p>Positive {good / (good + neutral + bad) * 100} %</p>
-
     </div>
   )
 }
