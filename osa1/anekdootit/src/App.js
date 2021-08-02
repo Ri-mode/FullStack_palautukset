a068a17eab2882902432b6275324d0338147f04c
Antti Kantola <antti.kantola@helsinki.fi>
2021-08-02 17:52:19 +0300

    osa1.14 Done!

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -16,14 +16,28 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ]
-   
+ 
   const [selected, setSelected] = useState(0)
+
+  const [votes, setVotes] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0))
+
+  const addVote = (props) => {
+    const copy = [ ...votes ]
+    copy[props] += 1
+    setVotes(copy)
+  }
 
   return (
     <div>
+      <h1>Anecdote of the day:</h1>
       {anecdotes[selected]}
+      <p>has {votes[selected]} votes</p>
       <p></p>
+      <Button handleClick={() => addVote(selected)} text='vote' />
       <Button handleClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))} text='next anecdote' />
+      <h1>Anecdote with most votes:</h1>
+      {anecdotes[votes.indexOf(Math.max(...votes))]}
+      <p>has {Math.max(...votes)} votes</p>
     </div>
   )
 }
