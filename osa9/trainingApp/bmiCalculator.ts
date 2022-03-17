2a1c12374dda7247beec52b37071fbdf907fadd5
Antti Kantola <antti.kantola@helsinki.fi>
2022-03-17 11:45:54 +0200

    Osa 9.1: Body mass index

---
+++ osa9/trainingApp/bmiCalculator.ts
@@ -0,0 +1,43 @@
+interface BmiValues {
+  value1: number;
+  value2: number;
+}
+
+const parseArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+
+
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / ((height / 100) * (height / 100));
+  console.log('bmi', bmi);
+
+  if (bmi < 16.0) return 'Underweight (severe thinness)';
+  if (bmi < 17.0) return 'Underweight (moderate thinness)';
+  if (bmi < 18.5) return 'Underweight (mild thinness)';
+  if (bmi < 25.0) return 'Normal (healthy weight)';
+  if (bmi < 30.0) return 'Overweight';
+  return 'Obese';
+}
+
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  console.log(calculateBmi(value1, value2));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage)
+}
